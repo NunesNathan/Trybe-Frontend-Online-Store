9c8a61ed0cb92289290e41bb039d0c7ee9ff0fc5
HiagoBriano <hiago.artist@hotmail.com>
2021-11-18 19:12:16 -0300

    G17 - Req 3 - concluido

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Home extends Component {
   render() {
     return (
       <main>
+        <Link to="/shoppingcart" data-testid="shopping-cart-button">
+          <button type="button">
+            <img src="https://cdn-icons-png.flaticon.com/512/1374/1374128.png" height="42" width="42" alt="carrinho" />
+          </button>
+        </Link>
+        <br />
         <span
           data-testid="home-initial-message"
         >
