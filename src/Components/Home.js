094818066cd479de0392cb82d555aadd5b03b061
NunesNathan <nunesxd7@hotmail.com>
2021-11-18 20:48:13 -0300

    G17 - Req 5 - Concluido

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,14 +1,57 @@
 import React, { Component } from 'react';
+import List from './List';
 
 export default class Home extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      inputValue: '',
+      toGo: false,
+    };
+  }
+
+  changeInput = (e) => {
+    this.setState({
+      inputValue: e.target.value,
+    });
+  }
+
+  enterFunc = (e) => {
+    e.preventDefault();
+    this.setState({
+      toGo: true,
+    });
+  }
+
   render() {
+    const { inputValue, toGo } = this.state;
     return (
       <main>
+        <form>
+          <label htmlFor="get">
+            <input
+              type="text"
+              data-testid="query-input"
+              value={ inputValue }
+              onChange={ this.changeInput }
+            />
+            <button
+              type="submit"
+              data-testid="query-button"
+              onClick={ this.enterFunc }
+            >
+              Buscar!
+            </button>
+          </label>
+        </form>
         <span
           data-testid="home-initial-message"
         >
           Digite algum termo de pesquisa ou escolha uma categoria.
         </span>
+        { toGo
+          && <List input={ inputValue } />}
       </main>
     );
   }
---
+++ src/Components/ItemCard.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import PropType from 'prop-types';
+
+export default class ItemCard extends Component {
+  render() {
+    const { title, thumbnail, price } = this.props;
+    return (
+      <li data-testid="product">
+        <span>{ title }</span>
+        <img src={ thumbnail } alt={ title } />
+        <span>{ price }</span>
+      </li>
+    );
+  }
+}
+
+ItemCard.propTypes = {
+  title: PropType.string.isRequired,
+  thumbnail: PropType.string.isRequired,
+  price: PropType.number.isRequired,
+};
