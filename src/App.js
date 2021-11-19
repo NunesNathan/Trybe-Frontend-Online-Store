c076c30199f467f6e391c4306f757311cfd1bf1a
HiagoBriano <hiago.artist@hotmail.com>
2021-11-18 21:50:28 -0300

    lint arrumado e css adicionado

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 import Redirect from './pages/Redirect';
-// import Home from './Home';
 
 function App() {
   return (
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import List from './List';
 import { Link } from 'react-router-dom';
+import List from './List';
 
 export default class Home extends Component {
   constructor() {
@@ -29,37 +29,38 @@ export default class Home extends Component {
     const { inputValue, toGo } = this.state;
     return (
       <main>
-      <span
-          data-testid="home-initial-message"
-        >
-          Digite algum termo de pesquisa ou escolha uma categoria.
-        </span>
-
-        <form>
-          <label htmlFor="get">
-            <input
-              type="text"
-              data-testid="query-input"
-              value={ inputValue }
-              onChange={ this.changeInput }
-            />
-            <button
-              type="submit"
-              data-testid="query-button"
-              onClick={ this.enterFunc }
+        <div className="menu">
+          <form>
+            <label htmlFor="get">
+              <input
+                type="text"
+                data-testid="query-input"
+                value={ inputValue }
+                onChange={ this.changeInput }
+              />
+              <button
+                type="submit"
+                data-testid="query-button"
+                onClick={ this.enterFunc }
+              >
+                Buscar!
+              </button>
+            </label>
+            <br />
+            <span
+              data-testid="home-initial-message"
             >
-              Buscar!
-            </button>
-          </label>
-        </form>
+              Digite algum termo de pesquisa ou escolha uma categoria.
+            </span>
+          </form>
 
-        <Link to="/shoppingcart" data-testid="shopping-cart-button">
-          <button type="button">
-            <img src="https://cdn-icons-png.flaticon.com/512/1374/1374128.png" height="42" width="42" alt="carrinho" />
-          </button>
-        </Link>
-        <br />
-        
+          <Link to="/shoppingcart" data-testid="shopping-cart-button">
+            <button type="button">
+              <img src="https://cdn-icons-png.flaticon.com/512/1374/1374128.png" height="42" width="42" alt="carrinho" />
+            </button>
+          </Link>
+        </div>
+
         { toGo
           && <List input={ inputValue } />}
       </main>
